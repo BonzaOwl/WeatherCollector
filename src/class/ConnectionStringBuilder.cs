b92dd32b146726c3f69d2777353e7ca48637c8d7
Bonza Owl <github@codenameowl.com>
2020-08-15 10:12:55 +0100

    Removed unused references, code clean

--- src/class/ConnectionStringBuilder.cs
+++ src/class/ConnectionStringBuilder.cs
@@ -1,29 +1,28 @@
-﻿using System.Data.Sql;
-using System.Data.SqlClient;
+﻿using System.Data.SqlClient;
 
 namespace WeatherCollectorDesktop
 {
     class ConnectionStringBuilder
-{
-        public static string ConnectionString()
-        {
-            SqlConnectionStringBuilder ConnString = new SqlConnectionStringBuilder();
+    {
+            public static string ConnectionString()
+            {
+                SqlConnectionStringBuilder ConnString = new SqlConnectionStringBuilder();
 
-            var server = Properties.Settings.Default.ServerName;
-            var databaseName = Properties.Settings.Default.DataBaseName;
-            var userName = Properties.Settings.Default.DatabaseUser;
-            var password = Properties.Settings.Default.DatabasePassword;
+                var server = Properties.Settings.Default.ServerName;
+                var databaseName = Properties.Settings.Default.DataBaseName;
+                var userName = Properties.Settings.Default.DatabaseUser;
+                var password = Properties.Settings.Default.DatabasePassword;
 
-            ConnString.DataSource = server;
-            ConnString.InitialCatalog = databaseName;
-            ConnString.UserID = userName;
-            ConnString.Password = password;
-            ConnString.IntegratedSecurity = true;
+                ConnString.DataSource = server;
+                ConnString.InitialCatalog = databaseName;
+                ConnString.UserID = userName;
+                ConnString.Password = password;
+                ConnString.IntegratedSecurity = true;
 
-            string ConnectionString = ConnString.ToString();
+                string ConnectionString = ConnString.ToString();
 
-            return ConnectionString;
+                return ConnectionString;
 
-        }
-}
+            }
+    }
 }
