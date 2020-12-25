c2ea5f6fef9291aa41dfcdfa369801221b8f5044
Bonza Owl <github@codenameowl.com>
2020-12-25 20:34:34 +0000

    Current Data Form
    Created a form to handle the current data

---
+++ src/currentdata.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace WeatherCollectorDesktop
+{
+    partial class CurrentData
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CurrentData));
+            this.txtCurrentData = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtCurrentData
+            // 
+            this.txtCurrentData.Location = new System.Drawing.Point(47, 37);
+            this.txtCurrentData.Multiline = true;
+            this.txtCurrentData.Name = "txtCurrentData";
+            this.txtCurrentData.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtCurrentData.Size = new System.Drawing.Size(712, 370);
+            this.txtCurrentData.TabIndex = 0;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(603, 413);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(684, 413);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(522, 413);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 23);
+            this.btnCopy.TabIndex = 3;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // CurrentData
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtCurrentData);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "CurrentData";
+            this.Text = "Current Data";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCurrentData;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnCopy;
+    }
+}
---
+++ src/currentdata.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeatherCollectorDesktop
+{
+    public partial class CurrentData : Form
+    {
+        public CurrentData()
+        {
+            InitializeComponent();
+            GetCurrentData();
+        }
+
+        private void GetCurrentData()
+        {
+            var sqldb_connection = ConnectionStringBuilder.ConnectionString();
+            using (SqlConnection con = new SqlConnection(sqldb_connection))
+                try
+                {
+                    con.Open();
+
+                    SqlCommand GetRunIDCommand = new SqlCommand("[dbo].[GetCurrentData]", con)
+                    {
+                        CommandType = CommandType.StoredProcedure
+                    };
+
+                    SqlDataReader dr = GetRunIDCommand.ExecuteReader();
+
+                    DataTable dt = new DataTable();
+
+                    dt.Load(dr);
+
+                    if (dt.Rows.Count > 0)
+                    {
+                        txtCurrentData.Text = dt.Rows[0]["CurrentData"].ToString();
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    _ = ex.Message.ToString();
+                }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtCurrentData.Text = string.Empty;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            
+            new WeatherCollector().Show();
+            Hide();
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(txtCurrentData.Text);
+        }
+    }
+}
