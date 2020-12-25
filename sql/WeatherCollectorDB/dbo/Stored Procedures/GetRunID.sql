39388dfb2d7c539636d1cae9a3a8fb9b20f98e84
Bonza Owl <github@codenameowl.com>
2020-12-25 16:54:57 +0000

    GetRunID
    Created stored procedure to get the recent RunID from the runData table.

---
+++ sql/WeatherCollectorDB/dbo/Stored Procedures/GetRunID.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROC dbo.GetRunID 
+
+AS
+
+BEGIN
+
+    DECLARE @RunID INT
+
+    SELECT @RunID = MAX(RunID) FROM dbo.runData
+
+    SET @RunID = @RunID + 1
+
+    SELECT @RunID as RunID 
+
+END
